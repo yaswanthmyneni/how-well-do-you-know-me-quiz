0e38cf2f6a2d94268e324f39897ecfbf5fd178b6
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2021-08-21 11:23:22 +0530

    refactor: added more questions

--- index.js
+++ index.js
@@ -1,46 +1,58 @@
-var rls = require('readline-sync');
+var rls = require("readline-sync");
 
 function log(data) {
   console.log(data);
 }
 
-var userName = rls.question('Your Name? ');
-log('Hello! ' + userName + ', Do You Know Yash?');
+var userName = rls.question("Your Name? ");
+log("Hello! " + userName + ", Do You Know Yaswanth?");
 
 var score = 0;
 
-function play(question, answer){
+function play(question, answer) {
   var userAnswer = rls.question(question);
 
-  if(userAnswer === answer){
-    log('You were Right!');
+  if (userAnswer === answer) {
+    log("You were Right!");
     score++;
-  } else{
-    log('You were Wrong!');
+  } else {
+    log("You were Wrong!");
     score--;
   }
 
-  console.log('current score: ', score);
-  log('............');
+  if (i === qna.length - 1) {
+    return console.log("YOUR TOTAL SCORE IS ", score);
+  }
+
+  console.log("current score: ", score);
+  log("............");
 }
 
 var qna = [
-
   {
-  question: 'what is my age? ',
-  answer: '21',
+    question: "what is yaswanth`s age? ",
+    answer: "21",
   },
 
   {
-  question: 'where i am from? ',
-  answer: 'tirupati',
+    question: "yaswanth`s hometown name? ",
+    answer: "tirupati",
   },
 
   {
-  question: 'where i am studying? ',
-  answer: 'chennai',
+    question: "yaswanth graduated from? ",
+    answer: "chennai",
   },
 
+  {
+    question: "what is yaswanth`s favorite color? ",
+    answer: "green",
+  },
+
+  {
+    question: "who is yaswanth`s favorite hero? ",
+    answer: "maheshbabu",
+  },
 ];
 
 for (var i = 0; i < qna.length; i++) {
